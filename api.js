a3726fcc459325f5f7f0610947bf388b8ed7b592
AbuFragrance <giuliano.developer@outlook.com>
2025-02-18 23:13:05 +0100

    .

--- api.js
+++ api.js
@@ -1,3 +1,25 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_k3v3HK3tKEqhlqFHPkwogW7PqEqhGhk",
+  authDomain: "vanlife-a1af5.firebaseapp.com",
+  projectId: "vanlife-a1af5",
+  storageBucket: "vanlife-a1af5.appspot.com",
+  messagingSenderId: "803007000356",
+  appId: "1:803007000356:web:446cd3a1ca406839258db1"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+// Refactoring the fetching functions below
+
+
+
+
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
