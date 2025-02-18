e00b06c3b6a8f9d3eeab9421fdc303a8710069b5
AbuFragrance <giuliano.developer@outlook.com>
2025-02-18 23:23:39 +0100

    Endlich rum laufen gebracht

--- api.js
+++ api.js
@@ -1,38 +1,47 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore/lite"
+// Firebase v9 Syntax (Modular SDK)
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_k3v3HK3tKEqhlqFHPkwogW7PqEqhGhk",
-  authDomain: "vanlife-a1af5.firebaseapp.com",
-  projectId: "vanlife-a1af5",
-  storageBucket: "vanlife-a1af5.appspot.com",
-  messagingSenderId: "803007000356",
-  appId: "1:803007000356:web:446cd3a1ca406839258db1"
-};
+    apiKey: "AIzaSyCsbWy9uUvIM2EeTMXX0ANHa38_AJT0Ha0",
+    authDomain: "vanslife-2db68.firebaseapp.com",
+    projectId: "vanslife-2db68",
+    storageBucket: "vanslife-2db68.firebasestorage.app",
+    messagingSenderId: "722437027609",
+    appId: "1:722437027609:web:e740b046a958e3f8547788"
+  };
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
 // Refactoring the fetching functions below
 
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef);
+    return snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+  }
 
 
 
 
-
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
+// export async function getVans(id) {
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
